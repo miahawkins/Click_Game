d7f712810eb73c23b90c5ba12ac731e695563ba4
Mia Hawkins <ilearnarcheology@aol.com>
2018-02-14 16:51:09 -0600

    add initial set up files

---
+++ click-game/src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () =>
+    <footer className="footer">
+        <div>Click Game - Mia Hawkins 2018</div>
+    </footer>;
+
+export default Footer;
